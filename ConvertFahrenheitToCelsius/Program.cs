633abcdca7ef7f08f4f89f776198199d321a1074
LW <LW@DESKTOP-COP8R75>
2016-05-16 07:20:20 +0800

    Add project files.

---
+++ ConvertFahrenheitToCelsius/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertFahrenheitToCelsius
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double celsius;
+            double fahrenheit;
+
+            Console.Write("Ange Temp i Grader i fahrenheit: ");
+            fahrenheit = double.Parse(Console.ReadLine());
+
+            celsius = (fahrenheit - 32) * 5 / 9;
+
+            Console.WriteLine($"Temperaturen {fahrenheit} F motsvarar {celsius:f1} c.");
+                
+        }
+    }
+}
